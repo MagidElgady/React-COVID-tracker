b12774511fd3be7cdaa883b7855c84ea19ce61f8
Magid Elgady <magid.elgady@outlook.com>
2020-08-21 20:25:22 +0100

    fixed chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
+    // Gets daily data and sets daily data with useState
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+    // Plots line graph for each country
+    const lineChart = (
+        // If length is 0, we get falsey 
+        // and chart won't be loaded
+        dailyData.length ? (
+            <Line
+                data={{
+                    labels: dailyData.map(({ date }) => date),
+                    datasets: [{
+                        data: dailyData.map(({ confirmed }) => confirmed),
+                        label: 'Infected',
+                        borderColor: '#3333ff',
+                        fill: true,
+                    }, {
+                        data: dailyData.map(({ deaths }) => deaths),
+                        label: 'Deaths',
+                        borderColor: 'red',
+                        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                        fill: true,
+                    }],
+                }}
+            />
+        ) : null
+    );
     return (
-        <h1>Chart</h1>
-    )
-}
+        <div className={styles.container}>
+            {lineChart}
+        </div>
+    );
+};
 
 export default Chart;
