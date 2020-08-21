80bba3eb4b77043aa0ade45d17758f9ef402505b
Magid Elgady <magid.elgady@outlook.com>
2020-08-21 15:55:08 +0100

    added cardscountrypickerchart to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+// Easy but messy way of import necessary files
+// import Cards from './components/Cards/Cards'
+// import Charts from './components/Charts/Charts';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1> App </h1>
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />
             </div>
         )
     }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// Use the following as a shorthand for importing
+// multiple files
+
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
